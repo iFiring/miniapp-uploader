5310d9c130e918c624428d8347f34b3065fb4183
iFiring <wchuan@drips.cn>
2021-04-06 20:06:48 +0800

    优化upload日志打印

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -7,7 +7,8 @@ import {setConfig as alipayInit, miniUpload as alipayUpload} from 'miniu'
 import {Config} from '../types'
 
 // eslint-disable-next-line no-console
-const log = console.log
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+const {log, table} = console
 
 export default class Upload extends Command {
   static description = '小程序上传命令'
@@ -19,7 +20,7 @@ export default class Upload extends Command {
     // flag with no value (-d, --description)
     description: flags.build({char: 'd', description: '版本描述', required: true})(),
     // flag with no value (-r, --robot)
-    robot: flags.build({char: 'r', description: 'CI机器人序号 0 - 30', parse: (robot: string): any => Number(robot)})(),
+    robot: flags.build({char: 'r', description: 'CI机器人序号 1 - 30', parse: (robot: string): any => Number(robot), default: 1})(),
   }
 
   static args = [{name: 'type', description: '小程序类型 wechat|alipay (非必填)'}]
@@ -41,11 +42,20 @@ export default class Upload extends Command {
     if (!type || type === 'wechat') {
       // 微信小程序上传
       log(chalk.yellow('微信小程序开始上传\n'))
+      const {wechat: wechatConf} = projectConfig
+      table && table({
+        appid: wechatConf.appid,
+        projectPath: wechatConf.projectPath,
+        privateKeyPath: wechatConf.privateKeyPath,
+        version: flags.version,
+        desc: flags.description,
+        robot: flags.robot || wechatConf.robot || 1,
+      })
       const project = new WechatInit({
-        appid: projectConfig.wechat.appid,
+        appid: wechatConf.appid,
         type: 'miniProgram',
-        projectPath: `${_PWD_}${projectConfig.wechat.projectPath}`,
-        privateKeyPath: `${_PWD_}${projectConfig.wechat.privateKeyPath}`,
+        projectPath: `${_PWD_}${wechatConf.projectPath}`,
+        privateKeyPath: `${_PWD_}${wechatConf.privateKeyPath}`,
         ignores: ['node_modules/**/*'],
       })
 
@@ -53,19 +63,21 @@ export default class Upload extends Command {
         const wechatUploadResult = await wechatUpload({
           project: project,
           version: flags.version || '0.0.0',
-          desc: flags.description || `CI机器人${flags.robot}的一段描述`,
-          robot: flags.robot || 0,
-          setting: projectConfig.wechat.setting,
-          onProgressUpdate: (progress: any) => {
-            const {
-              message, status,
-            } = progress
-            if (status === 'doing') {
-              log(`${message}上传中...`)
-            } else if (status === 'done') {
-              log(chalk.blue(`${message}上传成功!`))
+          desc: flags.description,
+          robot: flags.robot || wechatConf.robot || 1,
+          setting: wechatConf.setting,
+          onProgressUpdate: (progress: string | MiniProgramCI.ITaskStatus): void => {
+            if (typeof progress === 'string') {
+              log(progress)
             } else {
-              log(chalk.red(`上传失败:${message}`))
+              const {message, status} = progress
+              if (status === 'doing') {
+                log(`${message}上传中...`)
+              } else if (status === 'done') {
+                log(chalk.blue(`${message}上传成功!`))
+              } else {
+                log(chalk.red(`上传失败:${message}`))
+              }
             }
           },
         })
@@ -74,21 +86,30 @@ export default class Upload extends Command {
       } catch (error) {
         log(chalk.red(`微信上传失败:${error} \n`))
         log(error)
+        return
       }
     }
 
     if (!type || type === 'alipay') {
       // 支付宝小程序上传
       log(chalk.yellow('支付宝小程序开始上传\n'))
+      const {alipay: alipayConf} = projectConfig
+      table && table({
+        appId: alipayConf.appid,
+        toolId: alipayConf.toolId,
+        projectPath: alipayConf.projectPath,
+        privateKey: `${alipayConf.privateKey.slice(0, 10)}...`,
+        version: flags.version,
+      })
       alipayInit({
-        toolId: projectConfig.alipay.toolId,
-        privateKey: projectConfig.alipay.privateKey,
+        toolId: alipayConf.toolId,
+        privateKey: alipayConf.privateKey,
       })
       try {
         const alipayUploadResult = await alipayUpload({
-          project: `${_PWD_}${projectConfig.alipay.projectPath}`,
-          appId: projectConfig.alipay.appid,
-          packageVersion: flags.version || undefined, // 为空则线上包版本自增0.0.1
+          project: `${_PWD_}${alipayConf.projectPath}`,
+          appId: alipayConf.appid,
+          packageVersion: flags.version, // 为空则线上包版本自增0.0.1
           clientType: 'alipay',
           experience: true,
           onProgressUpdate: log,
@@ -98,7 +119,9 @@ export default class Upload extends Command {
       } catch (error) {
         log(chalk.red(`支付宝上传失败:${error} \n`))
         log(error)
+        return
       }
     }
+    log(chalk.green('\nDone!'))
   }
 }
